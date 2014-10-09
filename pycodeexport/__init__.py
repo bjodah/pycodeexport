261db9c3629c9ff48db2c9ef1201d247f3861536
Björn Dahlgren <bjodah@gmail.com>
2014-10-09 14:19:15 +0200

    Bump micro

--- pycodeexport/__init__.py
+++ pycodeexport/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 from .dist import PCEExtension, pce_build_ext
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 name_ = 'pycodeexport'
-version_ = '0.0.4'
+version_ = '0.0.5'
 
 classifiers = [
     "Development Status :: 2 - Pre-Alpha",
