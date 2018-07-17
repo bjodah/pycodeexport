d237c146f0f3d5661bc43dcc459bcdcbd5f4d113
Björn Dahlgren <bjodah@gmail.com>
2018-07-17 16:58:39 +0200

    Release docs for v0.1.2

--- CHANGES.rst
+++
@@ -1,4 +0,0 @@
-v0.1.2
-======
-- Change examples to use ``include_dirs``.
-- In ``.clean()```: Conditional access of attributes ``_written_files`` & ``_remove_tempdir_on_clean``.
--- scripts/run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-# Usage
-#   $ ./scripts/run_tests.sh
-# or
-#   $ ./scripts/run_tests.sh --cov pycvodes --cov-report html
-${PYTHON:-python} -m pytest --doctest-modules --pep8 --flakes $@
-python3 -m doctest README.rst
