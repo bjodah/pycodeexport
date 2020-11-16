9d7db8f4ba86e8e5e526efacd878b31403b75e90
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-11-16 18:45:23 +0100

    fix test_examples

--- examples/tests/test_examples.py
+++ examples/tests/test_examples.py
@@ -14,7 +14,7 @@ tests = glob.glob(os.path.join(os.path.dirname(__file__), '../*_main.py'))
 @pytest.mark.parametrize('pypath', tests)
 def test_examples(pypath):
     p = subprocess.Popen(
-        ['python3' if sys.version_info.major == 3 else 'python',
-         pypath, 'clean'],
-        cwd=os.path.join(os.path.dirname(__file__), '..'))
+        [sys.executable, pypath, 'clean'],
+        cwd=os.path.join(os.path.dirname(__file__), '..')
+    )
     assert p.wait() == 0  # SUCCESS==0
